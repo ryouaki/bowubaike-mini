7eff18155f10321cf5aa7a303eba4f89be79c199
Ya Hui Liang <46517115@qq.com>
2023-10-26 10:37:25 +0800

    备份代码

--- mini/app.js
+++ mini/app.js
@@ -1,18 +1,49 @@
 // app.js
+const SYSTEM_INFO = 'SYSTEM_INFO';
+
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    const _this = this;
     wx.getLocation({
       type: 'wgs84',
       success (res) {
-        console.log(res)
+        if (!_this.globalData.userInfo) {
+          _this.globalData.userInfo = {};
+        }
+        _this.globalData.userInfo.lat = res.latitude;
+        _this.globalData.userInfo.lon = res.longitude;
       }
      })
-     
+  },
+  onShow(options) {
+    this.initSystemInfo(options);
+  },
+  initSystemInfo(options) {
+    const systemInfo = wx.getStorageSync(SYSTEM_INFO);
+    const _this = this;
+    if (!systemInfo) {
+      const res = wx.getSystemInfoSync();
+      const system = {
+        SDKVersion: res.SDKVersion,
+        platform: res.platform,
+        brand: res.brand,
+        model: res.model,
+        system: res.system,
+        version: res.version,
+        screenHeight: res.screenHeight,
+        screenWidth: res.screenWidth,
+        safeArea: res.safeArea,
+        isDebug: false //res.platform === 'devtools'
+      }
+      wx.setStorageSync(SYSTEM_INFO, JSON.stringify(system));
+      _this.globalData.systemInfo = system;
+    } else {
+      this.globalData.systemInfo = JSON.parse(systemInfo);
+    }
+    if (options) {
+      this.globalData.$route = options.path;
+      this.globalData.$options = options;
+    }
   },
   globalData: {
     userInfo: null
--- mini/pages/index/index.js
+++ mini/pages/index/index.js
@@ -3,46 +3,27 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  navClick(e) {
+    const { dataset } = e.currentTarget;
+    const { method = "defaultHandle" } = dataset;
+    this[`${method}_handle`](e);
   },
-  // 事件处理函数
-  bindViewTap() {
+  defaultHandle (e) {
+
+  },
+  askme_handle (e) {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '/pages/askme/index',
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+  tourist_handle (e) {
+    wx.navigateTo({
+      url: '/pages/tourist/index',
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  museum_handle (e) {
+    wx.navigateTo({
+      url: '/pages/museum/index',
     })
   }
 })

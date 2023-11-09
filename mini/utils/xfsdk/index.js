b8a5bd02639659648b9b528a1971da7ab5884949
Ya Hui Liang <46517115@qq.com>
2023-11-09 09:59:51 +0800

    完成

--- mini/utils/xfsdk/index.js
+++ mini/utils/xfsdk/index.js
@@ -1,8 +1,9 @@
 const CryptoJS = require('../cryptojs/index.js');
 const base64 = require('../base64/index.js');
 
-const API_KEY = 'b5eebfa6e21f449c6491eb7caeffae60';
-const API_SECRET = 'YTVjYTdlOTJjZDNiNzRmZjU2NzQ4OGI2';
+const API_KEY = '';
+const API_SECRET = '';
+const APP_ID = '';
 
 function getWebsocketUrl() {
   var url = "wss://spark-api.xf-yun.com/v2.1/chat";
@@ -57,7 +58,7 @@ module.exports = {
       const uuid = app.globalData.code;
       const prompt = {
         "header": {
-          "app_id": '240be67e',
+          "app_id": APP_ID,
           "uid": uuid
         },
         "parameter": {
@@ -72,7 +73,7 @@ module.exports = {
             "text": [
               {
                 "role": "user",
-                "content": `假设你是导游或者博物馆解说或者动物学家植物学家，如果不是文物名称或者旅游景点名称或者动物名称或者植物名称，请返回"对不起，请提问与旅游景点和历史文物，动植物相关问题"，请用100字左右回答问题"${message}"`
+                "content": `假设你是导游或者博物馆解说或者动物学家植物学家，如果不是文物名称或者旅游景点名称或者动物名称或者植物名称，请返回"对不起，请提问与旅游景点和历史文物，动植物相关问题"，请回答问题"${message}"`
               }
             ]
           }
